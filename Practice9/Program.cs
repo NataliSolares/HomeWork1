56dc1285feb23cbe88afbd8166cbad00c0f22bce
Natali <n_sunberry@inbox.ru>
2022-08-08 14:43:53 +0300

    9 задача

---
+++ Practice9/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число: ");
+string s = Console.ReadLine();
+string r = "";
+r = r + s[0];
+r = r + s[2];
+Console.WriteLine(r);
+
