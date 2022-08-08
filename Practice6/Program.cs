003b547f109c542e62d1c819ea7ceb2e5e910f99
Natali <n_sunberry@inbox.ru>
2022-08-08 13:36:22 +0300

    6 задача

---
+++ Practice6/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число: ");
+string s = Console.ReadLine();
+int n = int.Parse(s);
+int r = n % 10;
+Console.WriteLine(r);
