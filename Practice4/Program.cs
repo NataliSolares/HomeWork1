67f71a9bdde1bb4e50e39afafab79135acd38198
Natali <n_sunberry@inbox.ru>
2022-08-08 09:56:22 +0300

    4 задача

---
+++ Practice4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить является ли число чётным
+int a = 7, b = 8;
+if(a % 2 == 0) 
+{
+    Console.WriteLine("a четное"); 
+}
+else 
+{
+    Console.WriteLine("a нечетное");
+}
+if(b % 2 == 0) 
+{
+    Console.WriteLine("b четное");
+}
+else 
+{
+    Console.WriteLine("b нечетное");
+}
