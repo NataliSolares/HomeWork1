319ba2204e6ca4f0db9b7c7aa526e6b7377d1aa2
Natali <n_sunberry@inbox.ru>
2022-08-08 14:30:02 +0300

    8 задача

---
+++ Practice8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int n = new Random().Next(10, 99);
+Console.Write("Выбрано число: ");
+Console.WriteLine(n);
+int n1 = n / 10;
+int n2 = n % 10;
+if(n1 > n2)
+{
+Console.Write(n1);
+}
+else {
+Console.Write(n2);
+}
