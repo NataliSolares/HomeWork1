ee49aec275269292b1262435b7fc09d45ff8ca27
Natali <n_sunberry@inbox.ru>
2022-08-08 09:16:55 +0300

    2 задача

---
+++ Practice2/Program.cs
@@ -0,0 +1,7 @@
+﻿// По заданному номеру дня недели вывести его название
+
+string[] array = {"пон", "вт", "ср", "чт", "пят", "субб", "воск"};
+Console.WriteLine("Ведите номер дня недели от 1 до 7 ");
+string input = Console.ReadLine();
+int numOfDay = int.Parse(input) - 1;
+Console.WriteLine(array[numOfDay]);
