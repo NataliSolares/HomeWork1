74e4185dd9d4cb4f667deb45a4f0a1a0feb4a760
Natali <n_sunberry@inbox.ru>
2022-08-10 23:22:27 +0300

    Задачи

---
+++ Practice011/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите трёхзначное число");
+int a = int.Parse(Console.ReadLine());
+int r = a % 10;
+
+if(a<100)
+{
+    Console.Write("У введённого числа нет третьей цифры");
+}
+else
+{
+    Console.WriteLine("Третья цифра числа: " + r);
+}
+
+    
